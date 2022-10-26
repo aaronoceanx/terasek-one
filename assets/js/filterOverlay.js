f853c466833114d42a3f93b7ff10c495e60997ab
AaronTan <aarontys1999@gmail.com>
2022-10-26 16:24:49 +0800

    style: filter overlay

---
+++ assets/js/filterOverlay.js
@@ -0,0 +1,7 @@
+function openNav() {
+  document.getElementById("filterOverlay").style.height = "100%";
+}
+
+function closeNav() {
+  document.getElementById("filterOverlay").style.height = "0%";
+}

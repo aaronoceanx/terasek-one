321eaba21de7deb33b7280d8d3008a667f8cc09b
AaronTan <aarontys1999@gmail.com>
2022-11-03 00:01:05 +0800

    style: done timeline , progress bar halfway

---
+++ assets/js/dashboard.js
@@ -0,0 +1,48 @@
+
+var value = document.querySelector(".progress").getAttribute('data-value');
+var left = document.querySelector('.progress-left .progress-bar');
+var right = document.querySelector('.progress-right .progress-bar');
+
+if (value > 0) {
+  if (value <= 50) {
+    right.style.transform='rotate(' + percentageToDegrees(value) + 'deg)'
+  } else {
+    right.style.transform='rotate(180deg)';
+    left.style.transform='rotate(' + percentageToDegrees(value - 50) + 'deg)'
+  }
+}
+
+function percentageToDegrees(percentage) {
+  return percentage / 100 * 360;
+}
+
+
+
+// document.querySelector(function() {
+
+//   document.querySelector(".progress").each(function() {
+
+//     var value = document.querySelector(this).attr('data-value');
+//     var left = document.querySelector(this).querySelector('.progress-left .progress-bar');
+//     var right = document.querySelector(this).querySelector('.progress-right .progress-bar');
+
+//     if (value > 0) {
+//       if (value <= 50) {
+//         right.css('transform', 'rotate(' + percentageToDegrees(value) + 'deg)')
+//       } else {
+//         right.css('transform', 'rotate(180deg)')
+//         left.css('transform', 'rotate(' + percentageToDegrees(value - 50) + 'deg)')
+//       }
+//     }
+
+//   })
+
+//   function percentageToDegrees(percentage) {
+
+//     return percentage / 100 * 360
+
+//   }
+
+// });
+
+

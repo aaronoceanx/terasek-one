2710338f667ec38e573e1bde81466c2cfa706cb2
AaronTan <aarontys1999@gmail.com>
2022-11-04 18:11:04 +0800

    style: complete chart

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -16,6 +16,19 @@ function percentageToDegrees(percentage) {
   return percentage / 100 * 360;
 }
 
+var value = document.querySelector(".progress.progress-active").getAttribute('data-value');
+var left = document.querySelector('.progress-left .progress-active.progress-bar');
+var right = document.querySelector('.progress-right .progress-active.progress-bar');
+
+if (value > 0) {
+  if (value <= 50) {
+    right.style.transform='rotate(' + percentageToDegrees(value) + 'deg)'
+  } else {
+    right.style.transform='rotate(180deg)';
+    left.style.transform='rotate(' + percentageToDegrees(value - 50) + 'deg)'
+  }
+}
+
 
 
 // document.querySelector(function() {
